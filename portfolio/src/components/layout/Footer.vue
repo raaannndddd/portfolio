c5deb6ff5988ad4d6cc6c35fecbc07f2b117d4d6
rhal2403 <rhal2403@uni.sydney.edu.au>
2025-03-04 23:42:24 +1100

    footer completed

---
+++ portfolio/src/components/layout/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <div class= "bg-yellow-200 block pt-8 pb-8 text-2xl">Let's connect!</div>
+        <div class= "flex pt-8 pb-8 justify-around text-2xl">
+            <a href="#Linkedin">Linkedin</a>
+            <a href="#Github">Github</a>
+            <a href="#Email">Email</a>
+        </div>
+    </div>
+</template>
