330e07549a65420301536011cac2e63b4a0ee4ed
rhal2403 <rhal2403@uni.sydney.edu.au>
2025-05-04 23:25:52 +1000

    deployment changes

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-import {fileURLToPath, URL} from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', "**/*.JPG"],
+  assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', '**/*.JPG'],
   plugins: [vue()],
-  resolve:{
-    alias:{
+  resolve: {
+    alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
