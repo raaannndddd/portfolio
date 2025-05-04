542d51afac77d2d3256953fc62033abb592eab4c
rhal2403 <rhal2403@uni.sydney.edu.au>
2025-05-04 23:19:23 +1000

    finally all working

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   assetsInclude: ['**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.gif', "**/*.JPG"],
   plugins: [vue()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss(), autoprefixer()],
-    },
-  },
   resolve:{
     alias:{
       '@': fileURLToPath(new URL('./src', import.meta.url))
