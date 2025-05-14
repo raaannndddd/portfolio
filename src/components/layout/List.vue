abaf8667b02101720c406c57fc5664502ca973d7
rhal2403 <rhal2403@uni.sydney.edu.au>
2025-05-14 12:33:33 +1000

    minor changes

--- src/components/layout/List.vue
+++ src/components/layout/List.vue
@@ -3,7 +3,11 @@
     <div class="mx-auto max-w-7xl px-6 lg:px-8">
       <div class="mx-auto max-w-2xl text-center">
         <h2 class="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Previous Works</h2>
-        <!-- <p class="mt-2 text-lg/8 text-gray-600"></p> -->
+        <p class="mt-2 text-lg/8 text-gray-600">
+        Check my 
+        <a href="https://github.com/raaannndddd" class="text-blue-500 hover:underline">GitHub</a>
+        for my latest projects!
+      </p>
       </div>
       <div class="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
         <article v-for="post in previous" :key="post.id" class="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80">
